3967b4701143796ba978eedd4b8f4b91da268f04
Freod <radek.paradecki@gmail.com>
2022-07-20 15:40:22 +0200

    Changed web application start config to Spring Boot.

--- api/src/main/java/com/epam/esm/Application.java
+++ api/src/main/java/com/epam/esm/Application.java
@@ -1,13 +1,11 @@
 package com.epam.esm;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        new SpringApplicationBuilder(Application.class)
-                .profiles("prod")
-                .run(args);
+        SpringApplication.run(Application.class, args);
     }
 }
